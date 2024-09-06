8d9b7993fa872296347c5455339f501713b34020
Thiago <thiagodeboliveira@outlook.com>
2024-09-06 01:17:50 -0300

    Progresso aula4

---
+++ dados.js
@@ -0,0 +1,22 @@
+let dados = [{
+    titulo: "Inteligencia Artifical",
+    descricao:"A inteligência artificial generativa, um ramo da IA que foca na criação de novos conteúdos, está revolucionando diversos setores. Essa tecnologia, capaz de gerar textos, imagens, músicas e até mesmo códigos de programação, aprende padrões complexos a partir de vastas quantidades de dados e utiliza esses conhecimentos para produzir resultados originais e criativos. Desde a geração de obras de arte únicas até a criação de assistentes virtuais mais sofisticados, a IA generativa está expandindo os limites da criatividade humana e abrindo portas para um futuro repleto de possibilidades inovadoras.",
+    link:"https://www.alura.com.br/artigos/inteligencia-artificial-ia?srsltid=AfmBOorniJ141NDSzfSYRwV0oXHOUgcFJCVgiU4IiIhg4ZP9Rr1x13Tf"
+},
+{
+    titulo: "Mergulhando no Metaverso",
+    descricao: "O metaverso, um universo virtual compartilhado, está redefinindo a forma como interagimos online. Essa nova realidade, que combina elementos do mundo real com o digital, oferece experiências imersivas e conecta pessoas de forma inovadora. Desde jogos e eventos sociais até trabalho e educação, o metaverso promete transformar a maneira como vivemos e trabalhamos.", 
+    link: "https://www.seusite.com/metaverso"
+},
+{
+    titulo: "Computação em Nuvem: O Futuro da Tecnologia,",
+    descricao: "A computação em nuvem revolucionou a forma como acessamos e armazenamos dados. Com ela, empresas e indivíduos podem utilizar recursos computacionais sob demanda, como servidores, armazenamento e bancos de dados, através da internet. Essa tecnologia oferece maior flexibilidade, escalabilidade e custo-benefício, impulsionando a inovação em diversos setores.",
+    link: "https://www.seusite.com/computacao-em-nuvem"
+},
+
+            
+            
+];
+
+
+
